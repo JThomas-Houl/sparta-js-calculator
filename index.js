1c5fee6ce6ffb9f1b02bd605fc245342ec65137f
Tech-A25 <tech-a25@tech-a25s-air.home>
2018-02-19 22:30:17 +0000

    funtions working

--- index.js
+++ index.js
@@ -46,14 +46,48 @@ var Mode = prompt("Which mode do you require?: Please enter (B) for Basic or A f
 
 if(Mode == "b" || Mode.charAt(0)){//checks the 1st letter in sting and forces to upper case
 	var Mode = Mode.charAt(0).toUpperCase();//get char at index 0 and changes to caps
-	console.log(Mode);
+	console.log(Mode); //for debuging only
 
 	if(Mode == "B"){
 	alert("Allowed functions: Additon, Multiplication, Division, Subtraction");
 
 }else{
 	alert("Allowed functions: Additon, Multiplication, Division, Subtraction, Power,Square Root");
+
 }
 }
+
+
+var Num_1= Number(prompt("Please enter your first number:"));
+var op = prompt("Please enter your desised operator. (Example: + , - , /");
+var Num_2= Number(prompt("Please enter your second number:"));
+
+
+
+console.log(add(Num_1,Num_2));
+
+/*
+for (var i = 0; i < Question.length; i++){
+
+  if(Question.charAt(i) == "+"){
+  	alert("add")
+  }
+  	
+}
+
+
+
+//switch statment
+
+console.log(Question);
+
+if(Question == "+"){
+	alert("add");
+}else{
+	prompt("not add")
+}
+
+*/
+
 
 
